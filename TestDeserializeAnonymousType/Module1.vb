fe4d581646e85958d8524b6eee2a940c1e153965
fransdihan <digb.adi@gmail.com>
2018-09-26 22:46:15 +0700

    commit all

--- TestDeserializeAnonymousType/Module1.vb
+++ TestDeserializeAnonymousType/Module1.vb
@@ -1,7 +1,16 @@
-﻿Module Module1
+﻿Imports Newtonsoft.Json
+
+Module Module1
 
     Sub Main()
+        Dim exampleJsonString = "{ ""UserId"": ""admin"", ""CompanyId"": ""RCD"" }"
 
+        Dim definition = New With {.CompanyId = String.Empty}
+
+        Dim oPayload = JsonConvert.DeserializeAnonymousType(exampleJsonString, definition)
+
+        Console.WriteLine(oPayload.CompanyId)
+        Console.ReadKey(True)
     End Sub
 
 End Module
